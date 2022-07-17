ea744c75d79de152e905585ca0ea605ea4584e30
Cinek <sikorskimarcin1@gmail.com>
2022-07-17 23:02:25 +0200

    fixed messages and statuses

--- products/views.py
+++ products/views.py
@@ -9,7 +9,7 @@ from products.models import Product,Warehouse
 
 # Create your views here.
 class WarehouseView(APIView):
-    def get(self, request):
+    def get(self):
         warehouses= Warehouse.objects.all()
         warehouse_serializer = WarehouseListSerializer(warehouses, many=True)
         return Response(warehouse_serializer.data)
@@ -26,7 +26,7 @@ class WarehouseView(APIView):
             return Response(serializer.errors)
 
 class WarehouseDetailView(APIView):
-    def delete(self, request, warehouse_id):
+    def delete(self, warehouse_id):
         try:
             warehouse = Warehouse.objects.get(pk=warehouse_id)
             warehouse.delete()
@@ -34,7 +34,7 @@ class WarehouseDetailView(APIView):
         except Warehouse.DoesNotExist:
             return Response({'status': 'failure', 'message': 'Warehouse does not exist '})
 
-    def get(self,request,warehouse_id):
+    def get(self,warehouse_id):
         warehouse = Warehouse.objects.get(pk=warehouse_id)
         warehouse_serializer = WarehouseListSerializer(warehouse)
         return Response(warehouse_serializer.data)
@@ -60,20 +60,20 @@ class ProductView(APIView):
             else:
                 return Response({'status': 'failure', 'error': serializer.errors})
         except Exception as e:
-            return Response({'status': 'failure', 'error': 'error'})
+            return Response({'status': 'failure', 'error': 'there was an error'})
 
 class ProductDetailView(APIView):
-    def get(self, request, product_id):
+    def get(self, product_id):
         product = Product.objects.get(pk=product_id)
         return Response(ProductDetailSerializer(product).data)
 
-    def delete(self, request, product_id):
+    def delete(self, product_id):
         try:
             product = Product.objects.get(pk=product_id)
             product.delete()
             return Response({'status': 'success', 'message': 'product deleted !'})
         except Product.DoesNotExist:
-            return Response({'status': 'failure', 'message': 'prodoct  not available!'})
+            return Response({'status': 'failure', 'message': 'prodoct  does not exist'})
 
     def put(self, request, product_id):
         data = request.data
